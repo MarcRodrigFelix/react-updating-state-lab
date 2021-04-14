d0295cae0ed902dfea75933242a266c304dee2ae
Marcos <m.rodrig.felix@gmail.com>
2021-04-13 21:59:47 -0700

    commit tree 60ebb85318ea01f447ecc977dcffba40e7807f65

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {
+  constructor(){
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  handleButton = () => {
+    this.setState(previousState => {
+      return {
+        timesClicked: previousState.timesClicked + 1
+      }
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleButton} >{this.state.timesClicked}</button>
+      </div>
+    )
+  }
+}
